7119342e4262f4e19720d645dbdea143a0533846
riimo-cmd <76709902+riimo-cmd@users.noreply.github.com>
2025-03-25 11:44:16 -0500

    basic html and some css done

---
+++ scripts/links.js
@@ -0,0 +1,11 @@
+
+
+console.log("hi")
+let nav = document.getElementById("navbar");
+
+nav.innerHTML += "<ul></ul>";
+
+/*<li><a href="./index.html">Home</a></li>
+        <li><a href="./index.html">About</a></li>
+        <li><a href="./index.html">Recipes</a></li>
+        <li><a href="./index.html">Submit</a></li>*/

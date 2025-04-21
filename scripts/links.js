f2b0118e9b2f7bc8889ef08a06022e31c60e8ca4
riimo-cmd <76709902+riimo-cmd@users.noreply.github.com>
2025-04-21 16:17:40 -0500

    added a section header and logic to change the header and footer

--- scripts/links.js
+++ scripts/links.js
@@ -1,11 +1,33 @@
 
 
-console.log("hi")
-let nav = document.getElementById("navbar");
+/*get elements*/ 
+let header = document.getElementById("header");
+let footer = document.getElementById("footer");
 
-nav.innerHTML += "<ul></ul>";
+/*link variables*/
+let home = "./index.html";
 
-/*<li><a href="./index.html">Home</a></li>
-        <li><a href="./index.html">About</a></li>
-        <li><a href="./index.html">Recipes</a></li>
-        <li><a href="./index.html">Submit</a></li>*/
+/*HTML to insert*/
+let headerContent = `<img src="../images/orange.png" alt="Dreamcicle Recipes" id="logo">
+        <h1>Dreamcicle Recipes</h1>
+        <nav id="navbar">
+            <ul>
+                <li><a href="${home}">Home</a></li>
+                <li><a href="${home}">Home</a></li>
+                <li><a href="${home}">Home</a></li>
+                <li><a href="${home}">Home</a></li>
+            </ul>
+        </nav>`;
+let footerContent = `<ul>
+        <li><a href="${home}">About</a></li>
+        <li><a href="${home}">Blog</a></li>
+        <li><a href="${home}">Sitemap</a></li>
+        <li><a href="${home}">Newsletter</a></li>
+        <li><a href="${home}">Contact</a></li>
+      </ul>
+      <p>copyright goes here</p>`;
+      
+/*Insert html*/
+header.insertAdjacentHTML("afterbegin", headerContent);
+footer.insertAdjacentHTML("afterbegin", footerContent);
+
---
+++ scripts/scripts.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+/** TODO: Handle newsletter input
+ * put form inputs into local storage
+ * clear the form div and replace it with a confirmation message that includes the inputs from local storage
+ * give custom error messages for invalid inputs
+*/
+
+
+/** TODO: Custom object
+ * review chapter 8
+*/
+
+
+
+

477e5135bbb638e5d7933199135e1901e7ab35d2
riimo-cmd <76709902+riimo-cmd@users.noreply.github.com>
2025-04-21 21:58:14 -0500

    added basic logic for the newsletter

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,14 +1,27 @@
 
 
-
-
-
-
 /** TODO: Handle newsletter input
  * put form inputs into local storage
  * clear the form div and replace it with a confirmation message that includes the inputs from local storage
  * give custom error messages for invalid inputs
+ * make sure it doesn't submit until you've added valid inputs
 */
+
+let firstNameBox = document.getElementById("name");
+let emailBox = document.getElementById("newsletter-email");
+let submitBtn = document.getElementById("newsletter-submit");
+let newsletter = document.getElementById("newsletter")
+let submitted = false;
+
+let firstName = firstNameBox.value;
+let email = emailBox.value;
+
+let welcomeTemplate = `<h3>Thanks for subscribing ${firstName}!</h3><p>A confirmation email will be sent to ${email} soon. Make sure to check your spam folder if you can't find it - sometimes letters get lost!`;
+
+submitBtn.addEventListener("click", () => {
+    newsletter.innerHTML = "";
+    newsletter.insertAdjacentHTML("beforeend", welcomeTemplate);
+});
 
 
 /** TODO: Custom object

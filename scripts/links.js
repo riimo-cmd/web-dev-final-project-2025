a8e89c62605700d298f379c863d10e7f1204b697
LilacAxolotl <76709902+LilacAxolotl@users.noreply.github.com>
2026-06-28 14:47:05 -0500

    Update links.js
    attempt 2 of fixing header image

--- scripts/links.js
+++ scripts/links.js
@@ -12,7 +12,7 @@ let home = "./index.html";
 let latest = "./index.html#latest";
 
 /*HTML to insert*/
-let headerContent = `<img src="./images/orange.png" alt="Dreamcicle Recipes" id="logo">
+let headerContent = `<img src="../web-dev-final-project-2025/images/orange.png" alt="Dreamcicle Recipes" id="logo">
         <h1>Dreamcicle Recipes</h1>
         <nav id="navbar">
             <ul>
